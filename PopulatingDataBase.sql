d1054bef8ba73d7d1bc1951f1633d3a5c3d88254
Krishna Singh Rajput <123626302+Krish-oo7@users.noreply.github.com>
2024-05-10 08:02:59 +0530

    Update and rename Final_Project_2.1.sql to PopulatingDataBase.sql

--- PopulatingDataBase.sql
+++ PopulatingDataBase.sql
@@ -1,14 +1,8 @@
-/*
-** Team: Final_Project_1
-** Authors Name-ID : Krishna Singh Rajput - 1229962853
-**                 : Nidhi Prakashbhai Mistry - 1230085261
-** Course: IFT/530
-** SQL Server version: Microsoft SQL Server 2022
-*/
-
-USE Final_Project_1;
+-- Populating data into all the tables
+USE Banking;
 GO
---Populate the Bank table
+  
+--Populating the Bank table
 INSERT INTO Bank (bank_id, bank_name) VALUES
 (113163645, 'State Bank of India'),
 (118440214, 'HDFC Bank'),
@@ -21,5 +15,3 @@ INSERT INTO Bank (bank_id, bank_name) VALUES
 (408987001, 'Desert Finance'),
 (495885336, 'Rajputana Bank');
 
---Testing insert query
-SELECT * FROM Bank;
